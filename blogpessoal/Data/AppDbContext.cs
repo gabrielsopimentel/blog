3b7a064122fbb0fb2bf93240ac048a47f6da3082
gabrielsopimentel <gabrielsopimentel@gmail.com>
2023-10-10 18:55:08 -0300

    offset now

--- blogpessoal/Data/AppDbContext.cs
+++ blogpessoal/Data/AppDbContext.cs
@@ -43,7 +43,7 @@ namespace blogpessoal.Data
                 //Se uma propriedade da Classe Auditable estiver sendo criada. 
                 if (insertedEntry is Auditable auditableEntity)
                 {
-                    auditableEntity.Data = new DateTimeOffset(DateTime.Now);
+                    auditableEntity.Data = DateTimeOffset.Now;
                 }
             }
 
@@ -56,7 +56,7 @@ namespace blogpessoal.Data
                 //Se uma propriedade da Classe Auditable estiver sendo atualizada.  
                 if (modifiedEntry is Auditable auditableEntity)
                 {
-                    auditableEntity.Data = new DateTimeOffset(DateTime.Now);
+                    auditableEntity.Data = DateTimeOffset.Now;
                 }
             }
 
